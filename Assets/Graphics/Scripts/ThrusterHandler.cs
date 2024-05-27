785ffaba5fab86b690dc59756faa52efe607d2bd
Swifter <swifter1243@gmail.com>
2024-05-27 16:52:56 -0400

    thruster exhaust

--- Assets/Graphics/Scripts/ThrusterHandler.cs
+++ Assets/Graphics/Scripts/ThrusterHandler.cs
@@ -6,19 +6,32 @@ public class ThrusterHandler : MonoBehaviour
 {
     public List<GameObject> thrusterPositions;
     public GameObject thrusterAsset;
-    List<GameObject> thrusters = new List<GameObject>();
+    List<ParticleSystem> thrusters = new List<ParticleSystem>();
 
     void Start()
     {
         thrusterPositions.ForEach(x => {
             var thruster = Instantiate(thrusterAsset, transform);
             thruster.transform.position = x.transform.position;
-            thrusters.Add(thruster); 
+            thrusters.Add(thruster.GetComponent<ParticleSystem>()); 
         });
     }
 
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(1)) {
+            SetThrusterOn(true);
+        }
+        if (Input.GetMouseButtonUp(1)) {
+            SetThrusterOn(false);
+        }
+    }
+
+    [System.Obsolete]
+    void SetThrusterOn(bool on)
+    {
+        thrusters.ForEach(x => {
+            x.enableEmission = on;
+        });
     }
 }
