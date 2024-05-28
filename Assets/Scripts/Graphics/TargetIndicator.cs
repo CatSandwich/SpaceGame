ad6af6832155e02ea5fcd9c1449c9138e627ae68
Swifter <swifter1243@gmail.com>
2024-05-27 20:16:41 -0400

    target indicator is now real

--- Assets/Scripts/Graphics/TargetIndicator.cs
+++ Assets/Scripts/Graphics/TargetIndicator.cs
@@ -13,12 +13,12 @@ namespace Graphics
         void Update()
         {
             var targetPos = target.transform.position;
-            var forward = transform.position - targetPos;
+            var forward = parent.transform.position - targetPos;
             transform.rotation = Quaternion.LookRotation(Vector3.up, forward);
 
             float length = Vector3.Magnitude(forward);
             transform.localScale = transform.localScale.WithY(length);
-            transform.localPosition = -forward / 2 + parent.transform.position;
+            transform.position = -forward / 2 + parent.transform.position;
         }
     }
 }
